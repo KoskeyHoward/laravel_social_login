ff5dd2e19433230e735ee4ca49dabf528784d15e
Le-Yo <koskeyhoward@gmail.com>
2019-01-30 10:41:07 +0300

    added facebook_id column on the users table

--- config/services.php
+++ config/services.php
@@ -35,4 +35,16 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    'github' => [
+    'client_id' => env('GITHUB_CLIENT_ID'),
+    'client_secret' => env('GITHUB_CLIENT_SECRET'),
+    'redirect' => 'http://127.0.0.1:8000',
+],
+
+'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => env('FACEBOOK_CALLBACK_URL'),
+    ],
+
 ];
