6697c78f9c1dbe96453ad420629cf290fa54e3e7
Kunal Punjabi <kunalspunjabi@yahoo.com>
2019-02-05 02:26:36 -0800

    add temporary Linkedin login button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -67,11 +67,31 @@
                               <a href="{{ url('auth/facebook') }}" class="btn btn-primary">Login with Facebook</a>
                             </div>
                         </div>
+
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
-                              <a href="{{ url('auth/linkedin') }}" class="btn btn-primary">Login with Linkedin</a>
+                                <a href="{{ url('auth/linkedin') }}">
+                                    <div class="linkedinBtn">
+                                      <div class='ico-wrapper'>
+                                        <svg class="ico" width="40px" height="40px" viewBox="0 0 40 40" version="1.1">
+                                          <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
+                                            <g id="LinkedIn" sketch:type="MSArtboardGroup" fill="#FFFFFF">
+                                              <g id="Fill-6-+-Fill-7" sketch:type="MSLayerGroup" transform="translate(6.000000, 5.000000)">
+                                                <path d="M3.44222222,0 C5.34,0 6.88,1.54111111 6.88,3.44 C6.88,5.34 5.34,6.88111111 3.44222222,6.88111111 C1.53666667,6.88111111 0,5.34 0,3.44 C0,1.54111111 1.53666667,0 3.44222222,0 L3.44222222,0 Z M0.471111111,9.48888889 L6.41,9.48888889 L6.41,28.5777778 L0.471111111,28.5777778 L0.471111111,9.48888889 Z"
+                                                id="Fill-6" sketch:type="MSShapeGroup"></path>
+                                                <path d="M10,9.47333333 L15.6866667,9.47333333 L15.6866667,12.0833333 L15.7688889,12.0833333 C16.56,10.5822222 18.4955556,9 21.3811111,9 C27.3888889,9 28.4988889,12.9522222 28.4988889,18.0933333 L28.4988889,28.5622222 L22.5666667,28.5622222 L22.5666667,19.2788889 C22.5666667,17.0655556 22.5288889,14.2177778 19.4844444,14.2177778 C16.3966667,14.2177778 15.9255556,16.63 15.9255556,19.1211111 L15.9255556,28.5622222 L10,28.5622222 L10,9.47333333"
+                                                id="Fill-7" sketch:type="MSShapeGroup"></path>
+                                              </g>
+                                            </g>
+                                          </g>
+                                        </svg>
+                                      </div>
+                                      Login with Linkedin
+                                    </div>
+                                </a>
                             </div>
                         </div>
+
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
                               <a href="{{ url('auth/github') }}" class="btn btn-primary">Login with Github</a>
